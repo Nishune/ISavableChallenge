a04f1bedb1d273d8c3643b33bdcc2dfee4e7f352
Rikard <rikardengstrom89@gmail.com>
2025-07-10 12:36:20 +0200

    challenge done!

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,8 +1,19 @@
 package dev.lpa;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Player p1 = new Player("Rikard", 100, 50);
+        Monster m1 = new Monster("Ragnaros", 100, 50);
+
+        System.out.println("Player " + p1);
+        System.out.println("Monster: " +m1);
+        List<String> savedData = p1.write();
+        List<String> savedDataMonster = m1.write();
+        System.out.println("Saved player " + savedData);
+        System.out.println("Saved monster " + savedDataMonster);
     }
 }
